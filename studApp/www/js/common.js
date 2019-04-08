67126d6da23f57527a786361e1cbdb3c5ab13197
hmm2309007850 <45476113+hmm2309007850@users.noreply.github.com>
2019-04-08 16:40:44 +0800

    学生端app

---
+++ studApp/www/js/common.js
@@ -0,0 +1,25 @@
+var ws = new WebSocket('ws://192.168.1.126:8083/app'); 
+// var ws = new WebSocket('ws://192.168.1.10:8181'); 
+ws.onopen = function(){
+    ws.send('已打开');
+};
+ws.onmessage = function(evt){
+    if(evt.data != '1'){
+        var index = top.layer.open({
+        type: 2,
+        area: ['90%', '86%'],
+        title: '答题页',
+        fixed: false, //不固定
+        maxmin: true,
+        content: evt.data,
+        cancel:function(){
+        if(!confirm('您确定要关闭此页面吗')){
+            return false;
+        }
+        }
+    });
+    }
+    if(evt.data == '1'){
+        layer.close(layer.index)
+    }    
+};
